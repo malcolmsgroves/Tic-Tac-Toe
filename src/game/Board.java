eda503bb5dddb858c9a938bc400093560e6aa07e
Malcolm Groves <macgroves@gmail.com>
2017-10-04 19:03:19 -0400

    Completed back-end code for tictactoe

--- src/game/Board.java
+++ src/game/Board.java
@@ -20,6 +20,7 @@ public class Board {
 	
 	Board () {
 			status = GameResult.IN_PROGRESS;
+			turn = Player.EX;
 			for(int i = 0; i < DIMENSION; ++i) {
 				for(int j = 0; j < DIMENSION; ++j) {
 					grid[i][j] = BLANK;
@@ -28,7 +29,46 @@ public class Board {
 	}
 	
 	public void play () {
+		Scanner scan = new Scanner(System.in);
 		
+		while(status == GameResult.IN_PROGRESS) {
+			
+			switch(turn) {
+			case EX:
+				System.out.println("X's turn");
+				break;
+			case OH: 
+				System.out.println("O's turn");
+				break;
+			}
+			System.out.println("Enter a row: ");
+			int row = scan.nextInt();
+			System.out.println("Enter a column: ");
+			int col = scan.nextInt();
+			
+			while(!make_play(row, col)) {
+				System.out.println("Invalid coordinates, enter a row" +
+						" column between 0 and 2, inclusive");
+				System.out.println("Enter a row: ");
+				row = scan.nextInt();
+				System.out.println("Enter a column: ");
+				col = scan.nextInt();
+			}
+			check_win();
+			print_board();
+		}
+		switch(status) {
+		case EX_WINS:
+			System.out.println("X wins!");
+			break;
+		case OH_WINS:
+			System.out.println("O wins!");
+			break;
+		default:
+			System.out.println("DRAW!");
+			break;
+		}
+		scan.close();
 	}
 	
 	public void check_win() {
@@ -63,7 +103,7 @@ public class Board {
 	public GameResult check_direction(int r_start, int c_start, int r_delta, int c_delta) {
 		
 		// keep track of a streak of the same char
-		boolean isStreak = true;
+		boolean is_streak = true;
 		
 		// check if the direction is null
 		if(r_delta == 0 && c_delta == 0) {
@@ -72,6 +112,7 @@ public class Board {
 		
 		int r_end = r_start + (DIMENSION - 1) * r_delta;
 		int c_end = c_start + (DIMENSION - 1) * c_delta;
+		
 		
 		// check if end point is on board
 		if(r_end < 0 || r_end >= DIMENSION || c_end < 0 || c_end >= DIMENSION) {
@@ -82,11 +123,12 @@ public class Board {
 		char curr_char = BLANK;
 		
 		for(int i = 0; i < DIMENSION; ++i) {
+			
 			int r_int = r_start + i * r_delta;
-			int c_int = c_start + i * r_delta;
+			int c_int = c_start + i * c_delta;
 			
 			if(grid[r_int][c_int] == BLANK) {
-				isStreak = false;
+				is_streak = false;
 			}
 			else if(curr_char == BLANK) {
 				curr_char = grid[r_int][c_int];
@@ -95,7 +137,9 @@ public class Board {
 				return GameResult.DRAW;
 			}
 		}
-		if(isStreak) {
+		
+		// if someone one
+		if(is_streak) {
 			winning_line[0][0] = r_start;
 			winning_line[0][1] = c_start;
 			winning_line[1][0] = r_end;
@@ -103,14 +147,17 @@ public class Board {
 			
 			switch(curr_char) {
 			case EX_MARK:
-				return GameResult.OH_WINS;
-			case OH_MARK:
 				return GameResult.EX_WINS;
+			case OH_MARK:
+				return GameResult.OH_WINS;
 			}
 		}
+		
+		// default: game is still going
 		return GameResult.IN_PROGRESS;
 	}
 	
+	// place a mark on the grid
 	public boolean make_play(int r, int c) {
 		if(grid[r][c] != BLANK) {
 			return false;
@@ -129,6 +176,11 @@ public class Board {
 	}
 	
 	public void print_board () {
-		
+		for(int r = 0; r < DIMENSION; r++) {
+			for(int c = 0; c < DIMENSION; c++) {
+				System.out.print(grid[r][c] + "\t");
+			}
+			System.out.println();
+		}
 	}
 }
---
+++ src/game/PlayTicTacToe.java
@@ -0,0 +1,11 @@
+package game;
+
+public class PlayTicTacToe {
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		board.play();
+
+	}
+
+}
